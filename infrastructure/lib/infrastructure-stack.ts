bd95247ebe820000798b142f5045748a045c8fe2
SJMiguel8 <bonilla.miguel.e@gmail.com>
2025-01-13 07:27:16 -0600

    f

--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -1,23 +1,11 @@
 import * as cdk from 'aws-cdk-lib';
-import * as cognito from 'aws-cdk-lib/aws-cognito';
+import * as amplify from '@aws-cdk/aws-amplify-alpha';
+import { Construct } from 'constructs';
 
 export class InfrastructureStack extends cdk.Stack {
-  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const userPool = cognito.UserPool.fromUserPoolId(
-      this,
-      'ExistingUserPool',
-      'us-east-2_m1pBRyoyM'  // Updated to your actual User Pool ID
-    );
-
-    const client = userPool.addClient('wavelength-web-client', {
-      generateSecret: false,
-      authFlows: {
-        adminUserPassword: true,
-        userPassword: true,
-        userSrp: true
-      }
-    });
+    // Add your infrastructure code here
   }
 }
